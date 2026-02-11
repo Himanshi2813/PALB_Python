3af73a72390048d0363c58c29897562597d91e27
Himanshi2813 <himanshikhantwal2803@gmail.com>
2026-02-11 20:46:22 +0530

    Add files via upload

---
+++ Restaurant.py
@@ -0,0 +1,12 @@
+print("Welcome to YUMMY RESTAURANT. Here is the menu.")
+print("Burger(Enter:b)")
+print("Chicken(Enter:c)")
+print("Pizza(Enter:p)")
+print("Choose a menu(enter:b,c,p)")
+a=input()
+if(a=='b'):
+    print("You chose Burger.")
+elif(a=='c'):
+    print("You chose Chicken.")
+elif(a=='p'):
+    print("You chose Pizza.")
